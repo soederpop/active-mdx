7a29b126080e4c2719b66952749071efa683e0bb
soederpop <jsoeder@codal.com>
2022-02-01 12:31:30 -0600

    CLI validate command can validate all of a specific model. e.g amdx validate stories

--- packages/core/src/cli/validate.mjs
+++ packages/core/src/cli/validate.mjs
@@ -1,8 +1,16 @@
 export default async function validate({ collection, ...argv }) {
   let pathIds = argv._.slice(1)
 
-  if (pathIds[0] === "all") {
-    pathIds = collection.available
+  if (pathIds.length === 1) {
+    if (pathIds[0] === "all") {
+      pathIds = collection.available
+    } else if (Object.keys(collection.modelAliases).indexOf(pathIds[0]) > -1) {
+      const modelName = collection.modelAliases[pathIds[0]]
+      pathIds = await collection
+        .query(modelName)
+        .fetchAll()
+        .then((r) => r.map((i) => i.id))
+    }
   }
 
   for (let pathId of pathIds) {
