02b9d2c012d0c2ff21139e95559987a3213032df
soederpop <jsoeder@codal.com>
2022-02-25 20:26:36 -0600

    not using the collection in the graphql endpoint

--- packages/software-project-demo-site/api/graphql.js
+++ packages/software-project-demo-site/api/graphql.js
@@ -42,11 +42,6 @@ export default async function handler(req, res) {
 
   await runMiddleware(req, res, cors)
 
-  if (!docs.loaded) {
-    console.log("loading docs")
-    await docs.load()
-  }
-
   if (!started) {
     console.log("starting graphql server")
     await apolloServer.start()
